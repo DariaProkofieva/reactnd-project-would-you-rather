f3d6a94b9127c432a92329be7a28407032eaf8af
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-09-03 14:29:29 +0200

    fear: created Question component

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
@@ -9,10 +10,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        <Dashboard/>
       </div>
     )
   }
 }
 
-export default connect()(App) 
+export default connect()(App)
---
+++ my-app/src/components/Question.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatQuestion } from '../utils/helpers'
+
+class Question extends Component {
+  render() {
+    const { question } = this.props
+    const { name, id, timestamp, optionOne, optionTwo, answer } = question
+    return (
+      <div className='question'>
+        <div><span>{name} asks:</span></div>
+        <div><h3>Would you rather</h3></div>
+        <div>{optionOne.text}</div>
+        <button>View Profile</button>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({authedUser, questions, users}, { id }) {
+  const question = questions[id]
+  return {
+    authedUser,
+    question: formatQuestion(question, users[question.author], authedUser)
+  }
+}
+
+export default connect(mapStateToProps)(Question)
