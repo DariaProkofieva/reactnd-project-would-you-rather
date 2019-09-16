8ed6d1476b297538eb9a638f69755e54e17d0c02
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-09-16 15:03:58 +0200

    feat: added unanswered list and answered list to Dashboard

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -6,21 +6,40 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h3 >Your Timeline</h3>
+        <h3>Answered Questions</h3>
         <ul>
-          {this.props.questionsIds.map((id) => (
-            <li key={id}>
-              <Question id={id}/>
+          {this.props.userAnsweredQuestions.map((key) => (
+            <li key={key}>
+              <Question id={key}/>
             </li>
           ))}
+        </ul>
+        <h3>Unanswered Questins</h3>
+        <ul>
+           {this.props.userUnansweredQuestions.map((key) => (
+            <li key={key}>
+              <Question id={key}/>
+            </li>
+          ))}
+
         </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ questions }) {
+function mapStateToProps ({ questions, authedUser, users }) {
+  const userQuestions = users[authedUser].answers
+  const userAnsweredQuestions = Object.keys(userQuestions)
+  const userUnansweredQuestions = Object.keys(questions).filter(key => userAnsweredQuestions.includes(key))
+  if (userUnansweredQuestions === undefined) {
+    return;
+  }
   return {
+    userUnansweredQuestions,
+    userAnsweredQuestions,
+    authedUser,
+    users,
     questionsIds: Object.keys(questions)
       .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
   }
