8df5dbc52a45cbfd35cc1af66d4cab1a3fc4b9f3
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-09-17 13:27:09 +0200

    fix: problem with unanswered and answered lists in Dashboard Component

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -3,38 +3,60 @@ import { connect } from 'react-redux'
 import Question from './Question'
 
 class Dashboard extends Component {
+  state = {
+    displayUnansweredQuestions:true
+  }
+  displayUnansweredQuestionsFunc = (e) => {
+    e.preventDefault();
+      this.setState({
+          displayUnansweredQuestions: true
+      })
+  }
+  displayAnsweredQuestionsFunc = (e) => {
+    e.preventDefault()
+      this.setState({
+          displayUnansweredQuestions: false
+      })
+  }
   render() {
-    return (
-      <div>
-        <h3>Answered Questions</h3>
-        <ul>
+    const displayUnansweredQuestions = this.state.displayUnansweredQuestions;
+      let display;
+      if (displayUnansweredQuestions) {
+        display = <ul>
+           {this.props.userUnansweredQuestions.map((key) => (
+             <li key={key}>
+              <Question id={key}/>
+            </li>
+          ))}
+        </ul>
+      } else {
+        display = <ul>
           {this.props.userAnsweredQuestions.map((key) => (
             <li key={key}>
               <Question id={key}/>
             </li>
           ))}
         </ul>
-        <h3>Unanswered Questins</h3>
-        <ul>
-           {this.props.userUnansweredQuestions.map((key) => (
-            <li key={key}>
-              <Question id={key}/>
-            </li>
-          ))}
-
-        </ul>
+      }
+    return (
+      <div>
+      <button onClick={this.displayUnansweredQuestionsFunc}>Unanswered Questions</button>
+      <button onClick= {this.displayAnsweredQuestionsFunc}>Answered Questions</button>
+      <div>{display}</div>
       </div>
-    )
+      )
   }
 }
 
 function mapStateToProps ({ questions, authedUser, users }) {
   const userQuestions = users[authedUser].answers
   const userAnsweredQuestions = Object.keys(userQuestions)
-  const userUnansweredQuestions = Object.keys(questions).filter(key => userAnsweredQuestions.includes(key))
+  const userUnansweredQuestions = Object.keys(questions).filter(key => !userAnsweredQuestions.includes(key))
   if (userUnansweredQuestions === undefined) {
     return;
   }
+  console.log(userAnsweredQuestions)
+  console.log(userUnansweredQuestions)
   return {
     userUnansweredQuestions,
     userAnsweredQuestions,
