2b6a834f64573ef33d53dc45bfb4629c89fe68d1
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-09-27 19:09:10 +0200

    style: added css for SingIn Component and changed avatars images

--- my-app/src/components/Results.js
+++ my-app/src/components/Results.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 class Results extends Component {
 
   render() {
-    const { optionOne, optionTwo, question, name } =this.props
+    const { optionOne, optionTwo, question, name, avatar } =this.props
     const allVotes = question.optionOne.votes.length + question.optionTwo.votes.length
     const optionOneVotes = question.optionOne.votes.length
     const optionTwoVotes = question.optionTwo.votes.length
@@ -13,6 +13,11 @@ class Results extends Component {
     const percentageOptionTwo = oneUser*optionTwoVotes
     return (
       <div className='results'>
+        <img
+          src={avatar}
+          alt={`Avatar of ${name}`}
+          className='avatarSmall'
+        />
         <div><span>Asked by {name}</span></div>
         <div><h2>Results:</h2></div>
         <h3>Would you rather {optionOne}?</h3>
@@ -34,10 +39,12 @@ function mapStateToProps({ questions, authedUser, users }, { id }) {
   const author = question.author
   const optionOne = question.optionOne.text
   const optionTwo = question.optionTwo.text
+  const avatar =users[authedUser].avatarURL
   return {
     optionOne,
     optionTwo,
     question,
+    avatar,
     name: author ===undefined ?  null: users[author].name
   }
 }
--- my-app/src/components/SingIn.js
+++ my-app/src/components/SingIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { setAuthedUser } from '../actions/authedUser'
+import { Redirect } from 'react-router-dom'
 
 class SingIn extends Component {
   state = {
@@ -19,21 +20,30 @@ class SingIn extends Component {
   }
 
   render() {
+    let authedUser = this.props
+    if (authedUser === true) {
+      return <Redirect to='/home'/>
+    }
     return(
-      <div>
+      <div className="singIn">
         <h1>Welcome to the Would you Rather App!</h1>
         <h2>Please sing in to continue</h2>
-        <h1>Sing in</h1>
-        <form onSubmit={this.handleSubmitUser}>
-          <select onChange={this.handleChange}>
+        <img
+          src="https://sun9-30.userapi.com/c851232/v851232949/1c5381/uABhMbHAaCQ.jpg"
+          alt="Avatar icon"
+          className='avatarIcon'
+        />
+        <h1 style={{color: "#7CA3FE"}}>Sing in</h1>
+        <form onSubmit={this.handleSubmitUser} className="singInForm">
+          <select onChange={this.handleChange} className="selectBtn">
             <option>Select User</option>
             {Object.keys(this.props.users).map((user) => (
-              <option key={this.props.users[user].id} value={this.props.users[user].id}>
+              <option className="option" key={this.props.users[user].id} value={this.props.users[user].id}>
                 {this.props.users[user].name}
               </option>
             ))}
           </select>
-          <button>Sing in</button>
+          <button className="btn">Sing in</button>
         </form>
       </div>
     )
