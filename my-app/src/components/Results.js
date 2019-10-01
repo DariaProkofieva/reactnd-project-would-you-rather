ab4f02d5e1d8fc92eb2f54ebb41da8b678b172cc
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-10-01 23:04:01 +0200

    style: added to Result Component

--- my-app/src/components/Results.js
+++ my-app/src/components/Results.js
@@ -1,6 +1,73 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 
+const StyledResult = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin:0;
+  padding:0;
+`
+const SmallAvatar = styled.img`
+  height: 150px;
+  border-radius: 50%;
+  margin: 10px;
+`
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-right:50px
+`
+const Question = styled.div`
+  background-color: ${props => props.theme.blue};
+  border-radius: 25px;
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  padding: 20px;
+`
+const TextArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const PercentageCircle= styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  background-color: ${props => props.theme.mainOrange};
+  font-weight: bold;
+  color: white;
+`
+const Percentage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 200px;
+`
+const H2 = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.mainOrange};
+  width: 400px;
+  border-radius: 25px;
+  padding: 10px;
+  color:white;
+  margin: 10px;
+`
 class Results extends Component {
 
   render() {
@@ -12,21 +79,30 @@ class Results extends Component {
     const percentageOptionOne = oneUser*optionOneVotes
     const percentageOptionTwo = oneUser*optionTwoVotes
     return (
-      <div className='results'>
-        <img
-          src={avatar}
-          alt={`Avatar of ${name}`}
-          className='avatarSmall'
-        />
-        <div><span>Asked by {name}</span></div>
-        <div><h2>Results:</h2></div>
-        <h3>Would you rather {optionOne}?</h3>
-        <p>{percentageOptionOne} %</p>
-        <p>{optionOneVotes} out of {allVotes} votes</p>
-        <h3>Would you rather {optionTwo}?</h3>
-        <p>{percentageOptionTwo} %</p>
-        <p>{optionTwoVotes} out of {allVotes} votes</p>
-      </div>
+      <StyledResult>
+        <Div>
+          <h3>Asked by {name}</h3>
+          <SmallAvatar src={avatar}
+                       alt={`Avatar of ${name}`}></SmallAvatar>
+        </Div>
+        <TextArea>
+          <H2>Results:</H2>
+          <Question>
+            <h3>Would you rather {optionOne}?</h3>
+            <Percentage>
+              <PercentageCircle><h2>{percentageOptionOne} %</h2></PercentageCircle>
+              <p>{optionOneVotes} out of {allVotes} votes</p>
+            </Percentage>
+          </Question>
+          <Question>
+            <h3>Would you rather {optionTwo}?</h3>
+            <Percentage>
+              <PercentageCircle><h2>{percentageOptionTwo} %</h2></PercentageCircle>
+              <p>{optionTwoVotes} out of {allVotes} votes</p>
+            </Percentage>
+          </Question>
+        </TextArea>
+      </StyledResult>
     )
   }
 }
