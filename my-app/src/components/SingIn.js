1cef58ebd1ce824866d5a44466e2dd9e2c7a410a
Daria Prokofieva <dashaprokofieva95@gmail.com>
2019-10-04 20:24:14 +0200

    fix: redirection to home after sing in

--- my-app/src/components/SingIn.js
+++ my-app/src/components/SingIn.js
@@ -1,17 +1,17 @@
-import React, { Component, useState } from 'react'
-import { connect } from 'react-redux'
-import { setAuthedUser } from '../actions/authedUser'
-import { Redirect } from 'react-router-dom'
-import styled from "styled-components"
+import React, { Component, useState } from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
+import { Redirect } from "react-router-dom";
+import styled from "styled-components";
 
 const StyledH1 = styled.h1`
-  color: ${props => props.theme.mainBlue}
-`
+  color: ${props => props.theme.mainBlue};
+`;
 const BigAvatar = styled.img`
   height: 150px;
   border-radius: 25px;
   margin: 10px;
-`
+`;
 const Button = styled.button`
   text-transform: uppercase;
   margin: 35px auto;
@@ -21,78 +21,83 @@ const Button = styled.button`
   font-size: 16px;
   width: 400px;
   background-color: ${props => props.theme.mainOrange};
-  border-radius:25px;
+  border-radius: 25px;
   user-select: none;
-  :focus {outline: none;}
-`
+  :focus {
+    outline: none;
+  }
+`;
 const StyledSingIn = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items:center;
-`
+  align-items: center;
+`;
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   user-select: none;
-  :focus {outline: none;}
-`
+  :focus {
+    outline: none;
+  }
+`;
 const StyledFormButton = styled.select`
   padding: 10px;
-  border: 1px solid rgba(0,0,0,.29);
+  border: 1px solid rgba(0, 0, 0, 0.29);
   cursor: pointer;
   background: #fff;
   font-size: 16px;
   width: 400px;
   border-radius: 25px;
   overflow: auto;
-  :focus {outline: none;}
-`
+  :focus {
+    outline: none;
+  }
+`;
 const SingIn = ({ users, authedUser, dispatch }) => {
-  const [selectedUser, setSelectedUser] = useState(null)
- 
-  const handleChange = ( event ) => {
-    setSelectedUser(event.target.value)
-  }
+  const [selectedUser, setSelectedUser] = useState(null);
 
-  const handleSubmitUser = ( event ) => {
-      event.preventDefault()
-      dispatch(setAuthedUser(selectedUser))
-  }
+  const handleChange = event => {
+    setSelectedUser(event.target.value);
+  };
 
-    if (authedUser === true) {
-      return <Redirect to='/home'/>
-    }
-    return(
-      <StyledSingIn>
-          <h1>Welcome to the Would you Rather App!</h1>
-          <h2>Please sing in to continue</h2>
-          <BigAvatar
-          src="https://sun9-30.userapi.com/c851232/v851232949/1c5381/uABhMbHAaCQ.jpg"
-          alt="Avatar icon"></BigAvatar>
-          <StyledH1>Sing in</StyledH1>
-          <StyledForm onSubmit={handleSubmitUser}>
-            <StyledFormButton onChange={handleChange}>
-                <option>Select User</option>
-                {Object.keys(users).map((user) => (
-                  <option key={users[user].id} value={users[user].id}>
-                    {users[user].name}
-                  </option>
-                ))}
-            </StyledFormButton>
-            <Button>Sing in</Button>
-          </StyledForm>
-      </StyledSingIn> 
-    )
-  }
+  const handleSubmitUser = event => {
+    event.preventDefault();
+    dispatch(setAuthedUser(selectedUser));
+  };
 
-function mapStateToProps({  users, authedUser }) {
+  return (
+    <StyledSingIn>
+      <h1>Welcome to the Would you Rather App!</h1>
+      <h2>Please sing in to continue</h2>
+      <BigAvatar
+        src="https://sun9-30.userapi.com/c851232/v851232949/1c5381/uABhMbHAaCQ.jpg"
+        alt="Avatar icon"
+      ></BigAvatar>
+      <StyledH1>Sing in</StyledH1>
+      <StyledForm onSubmit={handleSubmitUser}>
+        <StyledFormButton onChange={handleChange}>
+          <option>Select User</option>
+          {Object.keys(users).map(user => (
+            <option key={users[user].id} value={users[user].id}>
+              {users[user].name}
+            </option>
+          ))}
+        </StyledFormButton>
+        <Button>Sing in</Button>
+        {authedUser !== null ? <Redirect to="/home" /> : null}
+      </StyledForm>
+    </StyledSingIn>
+  );
+};
+
+function mapStateToProps({ users, authedUser }) {
   if (authedUser === undefined) {
     return;
   }
   return {
     users,
     authedUser
-  }
+  };
 }
-export default connect(mapStateToProps)(SingIn)
+export default connect(mapStateToProps)(SingIn);
